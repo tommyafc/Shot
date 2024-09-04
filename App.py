6914f573c6865183613739a2f30e8cd3c27c44e6
tommyafc <tommy1497@outlook.it>
2024-09-04 22:03:20 +0200

    Add files via upload

---
+++ App.py
@@ -0,0 +1,66 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm, colors
+import seaborn as sns
+import csv
+import glob
+from adjustText import adjust_text
+from matplotlib.cm import ScalarMappable
+from sklearn import preprocessing
+from sklearn.cluster import KMeans
+from sklearn.mixture import GaussianMixture
+from sklearn.metrics import adjusted_rand_score, silhouette_score
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.decomposition import PCA
+from yellowbrick.cluster import KElbowVisualizer, SilhouetteVisualizer
+from mplsoccer import PyPizza, Radar
+from matplotlib.offsetbox import OffsetImage, AnnotationBbox
+from sklearn.linear_model import LinearRegression
+import requests
+
+st.set_page_config(page_title='Onesto Analyst')
+
+st.title("ONESTO ANALYST")
+st.write('I have published my first webapp that calculates the number of expected shots per game for each team, based on data from previous league games.')
+st.write('This tool can be used to identify favourable odds and potentially gain an advantage over bookmakers.')
+st.write('Use it well. 🤗')
+
+st.image("https://i.postimg.cc/L8KqkdFC/Shot-Serie-A.png")
+
+with st.columns(3)[1]:
+    with st.form(key='columns_in_form'):
+     sq1 = st.selectbox("Team 1:",("Atalanta", "Bologna", "Cagliari","Como","Empoli","Fiorentina","Genoa","Hellas Verona","Inter","Juventus","Lazio","Lecce","Milan","Monza","Napoli","Parma","Roma","Torino","Udinese","Venezia"),)
+     sq2 = st.selectbox("Team 2:",("Atalanta", "Bologna", "Cagliari","Como","Empoli","Fiorentina","Genoa","Hellas Verona","Inter","Juventus","Lazio","Lecce","Milan","Monza","Napoli","Parma","Roma","Torino","Udinese","Venezia"),)
+     submitted = st.form_submit_button('Submit')
+
+
+url= 'https://fbref.com/en/comps/11/2024-2025/2024-2025-Serie-A-Stats'
+response = requests.get(url).text.replace('<!--', '').replace('-->', '')
+df = pd.read_html(response, header=1)[8]
+
+url= 'https://fbref.com/en/comps/11/2024-2025/2024-2025-Serie-A-Stats'
+response = requests.get(url).text.replace('<!--', '').replace('-->', '')
+df2 = pd.read_html(response, header=1)[9]
+
+df['Sh p90'] = df['Sh'] / df['90s']
+df['ShA p90'] = df2['Sh'] / df2['90s']
+
+df=df.set_index('Squad')
+casa= (df.loc[sq1,'Sh p90'] + df.loc[sq2,'ShA p90']) / 2
+ospite= (df.loc[sq2,'Sh p90'] + df.loc[sq1,'ShA p90']) / 2
+
+
+if sq1 == sq2:
+    st.write("")
+else:
+    st.write('Tiri',sq1,':', casa)
+    st.write('Tiri',sq2,':', ospite)
+    st.write('Tiri Totali:', casa+ospite)
+
+
+
+
+#streamlit run /Users/tommy14/Desktop/FDS/Onesto_analyst/App.py
+
